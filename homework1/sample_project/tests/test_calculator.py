99cea2b740e6b2aecbf11f94dcf5e3c93029c1ac
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-02-28 23:27:22 +0300

    Deleted 1 comments from test_calculator.py

--- homework1/sample_project/tests/test_calculator.py
+++ homework1/sample_project/tests/test_calculator.py
@@ -4,7 +4,7 @@ from EPAM_HW.homework1.sample_project.calculator import calc
 
 
 @pytest.mark.parametrize(
-    ["value", "expected_result"],  # changed a little syntax according rules
+    ["value", "expected_result"],
     [(65536, True), (12, False), (34, False)],  # added one more test to check function
 )
 def test_power_of_2(value: int, expected_result: bool):
