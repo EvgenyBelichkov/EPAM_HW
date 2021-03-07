49eb8d2a56dc6896c7a0034de0352911b4fbe3e3
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-03-07 12:07:39 +0300

    Upgraded code according recommendations

--- homework1/task05/maximal_subarray.py
+++ homework1/task05/maximal_subarray.py
@@ -6,26 +6,15 @@ Examples:
     nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
     result = 16
 """
-from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
     sum_list = []
     for i in range(len(nums)):
-        s = nums[i]
-        if i < len(nums) - k:
-            for j in range(1, k):
-                if s + nums[i + j] > s:
-                    s = s + nums[i + j]
-        else:
-            for j in range(1, len(nums) - i):
-                if s + nums[i + j] > s:
-                    s = s + nums[i + j]
-        sum_list.append(s)
+        sub_array = nums[i : i + k]
+        local_sum = sub_array[0]
+        for j in sub_array[1:]:
+            if local_sum < local_sum + j:
+                local_sum = local_sum + j
+            sum_list.append(local_sum)
     return max(sum_list)
-
-
-a = [-5, -1, 5, 7, 9, 11, -1, -10]
-b = 4
-
-print(find_maximal_subarray_sum(a, b))
