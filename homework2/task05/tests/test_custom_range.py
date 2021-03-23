57d361f690a37db752bd3f6f906b2d5f1405e326
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-03-23 14:38:55 +0300

    Test optimised according recommendations

--- homework2/task05/tests/test_custom_range.py
+++ homework2/task05/tests/test_custom_range.py
@@ -1,63 +1,43 @@
+from collections import namedtuple
 from string import ascii_lowercase
 
 import pytest
 
 from homework2.task05 import custom_range_function
 
+Testset = namedtuple("Testset", ["arguments", "expected_result"])
 
-@pytest.fixture
-def fixture_example1():
-    sequence = "123456789"
-    start = "1"
-    finish = "5"
-    expected_result = ["1", "2", "3", "4"]
-    yield sequence, start, finish, expected_result
+example_without_step_parameter = Testset(
+    arguments=dict(
+        sequence="123456789",
+        start="1",
+        finish="5",
+    ),
+    expected_result=["1", "2", "3", "4"],
+)
+example_only_finish_parameter = Testset(
+    arguments=dict(sequence=ascii_lowercase, finish="g"),
+    expected_result=["a", "b", "c", "d", "e", "f"],
+)
+example_full = Testset(
+    arguments=dict(
+        sequence=ascii_lowercase,
+        start="z",
+        finish="w",
+        step=-1,
+    ),
+    expected_result=["z", "y", "x"],
+)
+all_examples_to_test = [
+    example_full,
+    example_without_step_parameter,
+    example_only_finish_parameter,
+]
 
 
-def test_of_custom_sequence_example1(fixture_example1):
-    sequence, start, finish, expected_result = fixture_example1
+@pytest.mark.parametrize("testset", all_examples_to_test)
+def test_of_custom_sequence_example1(testset):
     assert (
-        custom_range_function.custom_range(
-            sequence, start=fixture_example1[1], finish=fixture_example1[2]
-        )
-        == expected_result
-    )
-
-
-@pytest.fixture
-def fixture_example2():
-    sequence = ascii_lowercase
-    finish = "g"
-    expected_result = ["a", "b", "c", "d", "e", "f"]
-    yield sequence, finish, expected_result
-
-
-def test_of_custom_sequence_example2(fixture_example2):
-    sequence, finish, expected_result = fixture_example2
-    assert (
-        custom_range_function.custom_range(sequence, finish=fixture_example2[1])
-        == expected_result
-    )
-
-
-@pytest.fixture
-def fixture_example3():
-    sequence = ascii_lowercase
-    start = "z"
-    finish = "w"
-    step = -1
-    expected_result = ["z", "y", "x"]
-    yield sequence, start, finish, step, expected_result
-
-
-def test_of_custom_sequence_example3(fixture_example3):
-    sequence, start, finish, step, expected_result = fixture_example3
-    assert (
-        custom_range_function.custom_range(
-            sequence,
-            start=fixture_example3[1],
-            finish=fixture_example3[2],
-            step=fixture_example3[3],
-        )
-        == expected_result
+        custom_range_function.custom_range(**testset.arguments)
+        == testset.expected_result
     )
