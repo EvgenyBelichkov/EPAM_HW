eac5d6dbc90041974af8987b808bc2847a9b45e1
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-03-23 14:14:45 +0300

    Test updated

--- homework2/task04/tests/test_cache_function.py
+++ homework2/task04/tests/test_cache_function.py
@@ -1,54 +1,29 @@
+from collections import namedtuple
+
 import pytest
 
 from homework2.task04 import cache_function
 
+Testset = namedtuple("Testset", ["function", "arguments"])
 
-@pytest.fixture
-def fixture():
-    def fun1(a, b):
-        return (a ** b) ** 2
-
-    cache_funk = cache_function.cache(fun1)
-    some = 2, 2
-    val_1 = cache_funk(*some)
-    val_2 = cache_funk(*some)
-    yield val_1, val_2
+test_examples = [
+    Testset(function=lambda a, b: (a ** b) ** 2, arguments=(2, 2)),
+    Testset(function=lambda a, b, c, d: a + b + c + d, arguments=(2, 2, 2, 2)),
+    Testset(function=lambda a, b: a + b, arguments=("emap", "training")),
+]
 
 
-def test_cache_function_example1(fixture):
-    val_1, val_2 = fixture
-    assert val_1 == val_2
+@pytest.mark.parametrize("testset", test_examples)
+def test_cache_function(testset):
+    call_number = 0
 
+    def function_with_call_count(*args, **kwargs):
+        nonlocal call_number
+        call_number += 1
+        return testset.function(*args, **kwargs)
 
-@pytest.fixture
-def fixture():
-    def fun2(a, b, c, d):
-        return a + b + c + d
-
-    cache_funk = cache_function.cache(fun2)
-    some = 2, 2, 2, 2
-    val_1 = cache_funk(*some)
-    val_2 = cache_funk(*some)
-    yield val_1, val_2
-
-
-def test_cache_function_example2(fixture):
-    val_1, val_2 = fixture
-    assert val_1 == val_2
-
-
-@pytest.fixture
-def fixture():
-    def fun3(a, b):
-        return a + b
-
-    cache_funk = cache_function.cache(fun3)
-    some = "EPAM", "training"
-    val_1 = cache_funk(*some)
-    val_2 = cache_funk(*some)
-    yield val_1, val_2
-
-
-def test_cache_function_example3(fixture):
-    val_1, val_2 = fixture
-    assert val_1 == val_2
+    decorated_function = cache_function.cache(function_with_call_count)
+    previous_result = decorated_function(*testset.arguments)
+    for i in range(10):
+        assert decorated_function(*testset.arguments) == previous_result
+        assert call_number == 1
