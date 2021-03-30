abfe820a1873d13597b8ac0f2c6e62b2bba8d25a
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-03-30 11:27:55 +0300

    Realisation of 7th homework, task 1

---
+++ homework7/task01/amount_of_occurrences.py
@@ -0,0 +1,38 @@
+"""
+Given a dictionary (tree), that can contains multiple nested structures.
+Write a function, that takes element and finds the number of occurrences
+of this element in the tree.
+Tree can only contains basic structures like:
+    str, list, tuple, dict, set, int, bool
+"""
+from typing import Any
+
+# Example tree:
+example_tree = {
+    "first": ["RED", "BLUE"],
+    "second": {
+        "simple_key": ["simple", "list", "of", "RED", "valued"],
+    },
+    "third": {
+        "abc": "BLUE",
+        "jhl": "RED",
+        "complex_key": {
+            "key1": "value1",
+            "key2": "RED",
+            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
+        },
+    },
+    "fourth": "RED",
+}
+
+
+def find_occurrences(tree: dict, element: Any) -> int:
+    number_of_occurrences = 0
+    if isinstance(tree, dict):
+        tree = tree.values()
+    for value in tree:
+        if value == element:
+            number_of_occurrences += 1
+        if isinstance(value, (list, set, dict, tuple)):
+            number_of_occurrences += find_occurrences(value, element)
+    return number_of_occurrences
