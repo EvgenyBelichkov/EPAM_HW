8ef7dcc7979cb8cd770494aabe7f448ebe7bd94b
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-03 16:30:57 +0300

    Code changed according recommendations

--- homework5/task02/tests/test_decorator.py
+++ homework5/task02/tests/test_decorator.py
@@ -3,26 +3,24 @@ from homework5.task02 import oop2
 # checking printing result, __doc__ and __name__  information
 
 
-@oop2.print_result
-def example_1(a, b):
-    """This function multiplies 'a' and 'b'"""
-    return a * b
+def test__return_doc_and_name_information_of_decorated_function():
+    @oop2.print_result
+    def example_1(a, b):
+        """This function multiplies 'a' and 'b'"""
+        return a * b
 
-
-def test_printing_result_of_example_1():
     assert example_1(2, 2) == 4
-
-
-def test_return_doc_and_name_information_of_example1():
     assert example_1.__doc__ == "This function multiplies 'a' and 'b'"
     assert example_1.__name__ == "example_1"
 
 
 # checking function without printing result
 
-without_print = example_1.__original_func
-print(type(without_print(1, 2)))
-
 
 def test_example1_without_printing():
-    assert type(without_print) == type(example_1)
+    @oop2.print_result
+    def printing_important_information():
+        print("EPAM is the best training ever")
+
+    without_print = printing_important_information.__original_func
+    assert type(without_print) == type(printing_important_information)
