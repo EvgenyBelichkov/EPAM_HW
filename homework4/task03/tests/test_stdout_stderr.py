1a6dcc8ff9251b223e8da62815d66ddf69fc2c02
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-03 14:27:18 +0300

    Code changed according requirements

--- homework4/task03/tests/test_stdout_stderr.py
+++ homework4/task03/tests/test_stdout_stderr.py
@@ -1,22 +1,29 @@
-import os
-import sys
-
-import pytest
-
 from homework4.task03.stdout_stderr import my_precious_logger
 
 
-@pytest.mark.parametrize(
-    "word, expected_result",
-    [("error: file not found", False), ("Ok", True), ("", True), ("1234567", True)],
-)
-def test_stdout_stderr_function(word, expected_result):
-    sys.stdout = open("stdout", "w")
-    sys.stderr = open("stderr", "w")
-    my_precious_logger(word)
-    sys.stdout.close()
-    sys.stderr.close()
-    size_stdout = os.path.getsize("stdout")
-    size_stderr = os.path.getsize("stderr")
-    a = size_stdout >= size_stderr
-    assert a == expected_result
+def test_stdout_stderr_function_example1(capsys):
+    my_precious_logger("error: file not found")
+    out, err = capsys.readouterr()
+    assert err == "error: file not found\n"
+    assert out == ""
+
+
+def test_stdout_stderr_function_example2(capsys):
+    my_precious_logger("ok")
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == "ok\n"
+
+
+def test_stdout_stderr_function_example3(capsys):
+    my_precious_logger("")
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == "\n"
+
+
+def test_stdout_stderr_function_example4(capsys):
+    my_precious_logger("1234567")
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == "1234567\n"
