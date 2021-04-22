2821cf3aa0f90f825598f5ab3480e1f8b9b036ae
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-22 14:34:08 +0300

    Code changed according requirements

--- homework7/task01/amount_of_occurrences.py
+++ homework7/task01/amount_of_occurrences.py
@@ -5,17 +5,19 @@ of this element in the tree.
 Tree can only contains basic structures like:
     str, list, tuple, dict, set, int, bool
 """
-from collections.abc import MutableMapping
+from collections.abc import Collection, Mapping
 from typing import Any
 
 
 def find_occurrences(tree: dict, element: Any) -> int:
     number_of_occurrences = 0
-    if isinstance(tree, MutableMapping):
+    if isinstance(tree, Mapping):
         tree = tree.values()
     for value in tree:
         if value == element:
             number_of_occurrences += 1
-        if isinstance(value, (list, set, dict, tuple)):
+        if isinstance(value, str):
+            continue
+        if isinstance(value, Collection):
             number_of_occurrences += find_occurrences(value, element)
     return number_of_occurrences
