5f5ba2758a55697cbee305b2f52619303db6fecf
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-23 13:23:57 +0300

    Code changed according requirements

--- homework7/task03/tests/test_tic_tac_toe.py
+++ homework7/task03/tests/test_tic_tac_toe.py
@@ -4,32 +4,63 @@ import pytest
 
 from homework7.task03.tic_tac_toe import tic_tac_toe_checker
 
-board = namedtuple("board", ["sequence", "expected_result"])
+board = namedtuple("board", ["sequence", "expected_result", "n"])
 
 data = [
     board(
-        sequence=[["-", "-", "o"], ["-", "x", "o"], ["x", " o", "x"]],
+        sequence=[
+            ["-", "-", "o", "x", "o"],
+            ["-", "x", "o", "o", "x"],
+            ["x", " o", "x", "x", "o"],
+            ["-", "x", "o", "o", "x"],
+            ["x", " o", "x", "x", "o"],
+        ],
         expected_result="unfinished!",
+        n=5,
     ),
     board(
-        sequence=[["-", "-", "o"], ["-", "o", "o"], ["x", "x", "x"]],
+        sequence=[
+            ["x", "o", "o", "x", "o"],
+            ["x", "x", "o", "o", "x"],
+            ["x", "o", "x", "x", "o"],
+            ["x", "x", "o", "o", "x"],
+            ["o", "o", "x", "x", "o"],
+        ],
         expected_result="x wins!",
+        n=5,
     ),
     board(
-        sequence=[["o", "-", "-"], ["x", "o", "-"], ["x", " -", "o"]],
+        sequence=[
+            ["x", "o", "o", "x", "o"],
+            ["o", "x", "o", "o", "o"],
+            ["x", "o", "x", "o", "o"],
+            ["x", "x", "o", "o", "x"],
+            ["o", "o", "x", "x", "o"],
+        ],
         expected_result="o wins!",
+        n=5,
     ),
     board(
-        sequence=[["x", "o", "o"], ["o", "x", "x"], ["x", " x", "o"]],
-        expected_result="draw!",
-    ),
-    board(
-        sequence=[["-", "-", "-"], ["-", "-", "-"], ["-", " -", "-"]],
-        expected_result="unfinished!",
+        sequence=[
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "o", "o", "o"],
+            ["-", "-", "-", "-", "-", "-", "x", "x", "x", "x"],
+        ],
+        expected_result="x wins!",
+        n=10,
     ),
 ]
 
 
 @pytest.mark.parametrize("examples", data)
 def test_tic_tac_toe_function(examples):
-    assert tic_tac_toe_checker(examples.sequence) == examples.expected_result
+    assert (
+        tic_tac_toe_checker(examples.sequence, examples.n) == examples.expected_result
+    )
--- homework7/task03/tic_tac_toe.py
+++ homework7/task03/tic_tac_toe.py
@@ -18,29 +18,43 @@ Example:
 from typing import List
 
 
+def getting_possible_boards(board, n):
+    """Function that return all possible
+    boards 4x4 size"""
+    boards = []
+    for k in range(n - 3):
+        for j in range(n - 3):
+            possible_board = [i[j : 4 + j] for i in board[k : 4 + k]]
+            boards.append(possible_board)
+    return boards
+
+
 def checking_function(horizontal_lines):
     result = None
     for i in horizontal_lines:
         if "-" in i:
             result = "unfinished!"
             continue
-        elif i[0] == i[1] == i[2]:
+        elif i[0] == i[1] == i[2] == i[3]:
             return i[0] + " wins!"
         elif result is None:
             result = "draw!"
     return result
 
 
-def tic_tac_toe_checker(board: List[List]) -> str:
-    result = [checking_function(board)]
+def tic_tac_toe_checker(board: List[List], n) -> str:
+    possible_boards = getting_possible_boards(board, n)
+    result = []
+    for board in possible_boards:
+        result.append(checking_function(board))
 
-    vertical_lines = zip(board[0], board[1], board[2])
-    result.append(checking_function(list(vertical_lines)))
+        vertical_lines = zip(board[0], board[1], board[2], board[3])
+        result.append(checking_function(list(vertical_lines)))
 
-    left_diagonal = [board[0][0], board[1][1], board[2][2]]
-    right_diagonal = [board[0][2], board[1][1], board[2][0]]
-    diagonals = [left_diagonal, right_diagonal]
-    result.append(checking_function(diagonals))
+        left_diagonal = [board[0][0], board[1][1], board[2][2], board[3][3]]
+        right_diagonal = [board[0][3], board[1][2], board[2][1], board[3][0]]
+        diagonals = [left_diagonal, right_diagonal]
+        result.append(checking_function(diagonals))
 
     possible_results = ["x wins!", "o wins!", "unfinished!", "draw!"]
 
