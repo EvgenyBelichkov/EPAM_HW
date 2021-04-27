151db0e1bf7fcf68913e95fc51f32b009c615b02
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-27 10:53:29 +0300

    Realisation of 4th homework, task 1

--- homework12/data_base_project/university/models.py
+++ homework12/data_base_project/university/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=250)
 
 
