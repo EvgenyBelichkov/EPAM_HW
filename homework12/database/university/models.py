20e88c95a5aa22b4318aba161d46cc2541e64ea0
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-27 12:02:48 +0300

    Realisation of 12th homework

--- homework12/database/university/models.py
+++ homework12/database/university/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+
+
+class Homework(models.Model):
+    text = models.CharField(max_length=250)
+    dl = models.DateTimeField()
+    time_created = models.DateTimeField()
+
+
+class HomeworkResult(models.Model):
+    author = models.ForeignKey(Student, on_delete=models.CASCADE)
+    homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
+    solution = models.CharField(max_length=250)
+    time_created = models.DateTimeField()
