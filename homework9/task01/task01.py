44bf3d715d0c98eb5666970c83d331c6d2488387
Evgeny Belichkov <evgeny@DESKTOP-6CKKHPL.localdomain>
2021-04-30 10:20:23 +0300

    code changed according recommendations

--- homework9/task01/task01.py
+++ homework9/task01/task01.py
@@ -11,15 +11,39 @@ file2.txt:
 >> list(merge_sorted_files(["file1.txt", "file2.txt"]))
 [1, 2, 3, 4, 5, 6]
 """
-from heapq import merge
 from pathlib import Path
 from typing import Iterator, List, Union
 
 
+def merging_lists(lists):
+    result_list = []
+    while lists:
+        for list in lists:
+            if (
+                lists.index(list) == 0 and len(list) > 0
+            ):  # минимальным принимаю первый элемент вложенного списка
+                minimum = list.pop(0)
+                result_list.append(minimum)
+            for el in list:
+                if el >= minimum:
+                    break
+                else:  # если находится элемент, меньше установленного мимнимума - минимум заменяется
+                    lists[0].insert(0, minimum)
+                    minimum = list.pop(0)
+                    result_list[-1] = minimum
+        lists = [
+            list for list in lists if list
+        ]  # удаляю пустые списки, которые больше не используются
+    return result_list
+
+
 def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
-    result = []
+    lists = []
     for file_name in file_list:
         with open(file_name) as numbers:
+            sub_list = []
             for line in numbers:
-                result = merge(result, [int(line)])
-    return result
+                sub_list.append(int(line.strip("\n")))
+            lists.append(sub_list)
+    iterator = iter(merging_lists(lists))
+    return iterator
